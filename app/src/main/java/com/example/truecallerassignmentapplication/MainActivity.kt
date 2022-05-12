edea1aa330992a836133386c78bf47cc4bb1ec78
ElHadedyY <yassin.elhadedy@vodafone.com>
2022-05-12 12:55:24 +0200

    shared view model

--- app/src/main/java/com/example/truecallerassignmentapplication/MainActivity.kt
+++ app/src/main/java/com/example/truecallerassignmentapplication/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.truecallerassignmentapplication
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import com.example.truecallerassignmentapplication.databinding.ActivityMainBinding
 import com.example.truecallerassignmentapplication.ui.MainViewModel
 import com.example.truecallerassignmentapplication.ui.NavigationJourney
+import com.example.truecallerassignmentapplication.ui.NavigationJourney.*
 import com.example.truecallerassignmentapplication.ui.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,18 +27,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun observer() {
-        mainViewModel.navigationJourney.observe(this) {
+        mainViewModel.navigationJourneyLiveData.observe(this) {
             it?.let {
                 when (it.peekContent().navigationJourney) {
                     NavigationJourney.RUN_SIMULTANEOUSLY -> openFragment(
                         HomeFragmentDirections.actionHomeFragmentToRunSimultaneouslyFragment()
                     )
-                    NavigationJourney.RUN_SERIES -> openFragment(
-                        HomeFragmentDirections.actionHomeFragmentToRunSeriesFragment()
+                    NavigationJourney.RUN_SIMULTANEOUSLY_SECOND_APPROACH -> openFragment(
+                        HomeFragmentDirections.actionHomeFragmentToRunSimultaneouslySecondApproachFragment()
                     )
+                    else -> TODO()
                 }
             }
         }
+
+        //second approach with state flow
+//        lifecycleScope.launchWhenStarted {
+//            mainViewModel.navigationJourneySecondApproach.collect {
+//                when(it.navigationJourney){
+//                    RUN_SIMULTANEOUSLY -> openFragment(
+//                        HomeFragmentDirections.actionHomeFragmentToRunSimultaneouslyFragment()
+//                    )
+//                    RUN_SIMULTANEOUSLY_SECOND_APPROACH -> openFragment(
+//                        HomeFragmentDirections.actionHomeFragmentToRunSimultaneouslySecondApproachFragment()
+//                    )
+//
+//                }
+//            }
+//        }
     }
 
     private fun openFragment(navigationDirection: NavDirections) {
--- app/src/main/java/com/example/truecallerassignmentapplication/ui/MainViewModel.kt
+++ app/src/main/java/com/example/truecallerassignmentapplication/ui/MainViewModel.kt
@@ -1,15 +1,19 @@
 package com.example.truecallerassignmentapplication.ui
 
 import android.os.Bundle
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.fashhub.camxviewmlapplication.ui.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 enum class NavigationJourney(val value: Int) {
+    NOTHING(-1),
     RUN_SIMULTANEOUSLY(0),
-    RUN_SERIES(1);
+    RUN_SIMULTANEOUSLY_SECOND_APPROACH(1);
 
     companion object {
         fun valueOf(value: Int) = values().find { it.value == value }
@@ -20,8 +24,14 @@ class Navigator(val navigationJourney: NavigationJourney, val bundle: Bundle? = 
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
-    val navigationJourney = MutableLiveData<Event<Navigator>>(null)
+    val _navigationJourneyLiveData = MutableLiveData<Event<Navigator>>(null)
+    val navigationJourneyLiveData:LiveData<Event<Navigator>> =_navigationJourneyLiveData
 
+    private val _navigationJourneySecondApproach = MutableStateFlow(Navigator(NavigationJourney.NOTHING))
+    val navigationJourneySecondApproach:StateFlow<Navigator> =_navigationJourneySecondApproach
 
-
+    fun postJourney(navigator:Navigator){
+        _navigationJourneyLiveData.postValue(Event(navigator))
+        _navigationJourneySecondApproach.value = navigator
+    }
 }
